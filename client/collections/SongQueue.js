bd163db1be9b8cc1774352e99ad53042ecd1eeee
asponring <asponring@gmail.com>
2015-04-08 17:35:15 -0700

    Fixed some tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,34 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function(params){
+    this.on('add', function(song){
+      if (this.length === 1) {
+        this.playFirst(song);
+      }
+    }, this);
+
+    this.on('ended', function(song){
+      this.shift();
+
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+
+      // if (this.length >= 1) {
+      //   this.playFirst();
+      // }
+    }, this);
+
+  },
+
+
+  playFirst: function() {
+    console.log('played');
   }
 
 });
